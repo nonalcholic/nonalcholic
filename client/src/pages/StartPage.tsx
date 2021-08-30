8ee07319f8bf7acf8e248da9c4460ce1f6a4a1c9
leeminxji <leeminxji@gmail.com>
2021-08-30 21:00:24 +0900

    Connect between client and server

--- client/src/pages/StartPage.tsx
+++ client/src/pages/StartPage.tsx
@@ -5,18 +5,15 @@ interface Props {}
 const StartPage: React.FC<Props> = (props) => {
   const history = useHistory();
 
+  const onClickFunction = async () => {
+    const res = await fetch("http://localhost:9999/");
+    console.log(res.body);
+    const text = await res.text();
+    console.log("text,", text);
+  };
   return (
     <>
-      <button
-        onClick={() => {
-          fetch("/").then((res) => {
-            console.log("hello server");
-            console.log(res);
-          });
-        }}
-      >
-        요청 보내기
-      </button>
+      <button onClick={() => onClickFunction()}>요청 보내기</button>
       <button onClick={() => history.push("/progress")}>시작하기</button>
     </>
   );
--- server/main.go
+++ server/main.go
@@ -7,7 +7,13 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, rq *http.Request) {
-		w.Write([]byte("Babo!"))
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if rq.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Headers", "Authorization") // You can add more headers here if needed
+		} else {
+			// Your code goes here
+			w.Write([]byte("Babo!"))
+		}
 	})
 	http.ListenAndServe(":9999", nil)
 }
