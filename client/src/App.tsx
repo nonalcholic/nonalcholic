043a79db13bac59e7868686804963d15eca30101
leeminxji <leeminxji@gmail.com>
2022-02-25 18:54:10 +0900

    Initialize gh-pages

--- client/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import "./App.scss";
-import RootRouter from "./routers/RootRouter";
-
-function App() {
-  return <RootRouter />;
-}
-
-export default App;
--- client/src/utils/utils.env.ts
+++
@@ -1,3 +0,0 @@
-export const IP_ADDRESS = process.env.REACT_APP_IP_ADDRESS;
-export const SERVER_PORT = process.env.REACT_APP_SERVER_PORT;
-export const CLIENT_PORT = process.env.REACT_APP_CLIENT_PORT;
